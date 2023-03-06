d8e5d56cdbfc363372d7605746f3b5174864a980
Gaku Tokusumi <gakutokusumi@mac.com>
2023-03-06 10:04:49 -0500

    reworked logic and added helper functions

--- HKRECON.py
+++ HKRECON.py
@@ -8,9 +8,13 @@ Will pull through the boards downloaded from Infor HMS and create a new excel fi
 
 Dependencies:
 Pandas - creates tables from excel to support with data processing
-Openpyxl 
+Openpyxl - excel support
 '''
-
+kings = ['GK','AGK','B5GK','DGK','STK']
+queens = ['GQQ','GTT','STB','DCGQQ','ASJQQ']
+suitesK = ['SGK','STE1']
+SuitesS = 'STE2'
+suitesQ = ['SGQQ','ASGQQ']
 
 
 # pull data from boards
@@ -21,24 +25,39 @@ def pull(name):
 # clean dataframe 
 def clean(df):
     output = df[['Room Type','Employee Assigned','Room Points','Service Type','Action']]
-    return output.sort_values(by='Employee Assigned')
+    output.sort_values(by='Employee Assigned')
+    return dict(tuple(output.groupby(by='Employee Assigned')))
 
 
 # calculates workload by employee 
-def process(df):
-    pass
+def process(dict):
+    finCount = []
+    for i in dict:
+        finCount.append(counter(i))
+        
 
 # returns count per employee
-def counter(df):
-    pass
+def counter(dict):
+    count = initDict()
+
+
+# initializes Dictionary for room count
+def initDict():
+    # change according to new set up 
+    return {'King Checkout':0,'King Stayover':0,'Queen Checkout':0,'Queen Stayover':0}
 
 # creates workbook
 def create():
     return pxl.Workbook()
 
 # saves calculations in excel format
-def save(df):
+def save(pxlobj):
+    pxlobj.save("HE REPORT.xlsx")
+
+# takes in list and adds each item to spreadsheet
+def fill(lst):
     pass
+
 
 # Driver for program
 def main():
@@ -46,4 +65,4 @@ def main():
 
 
 if __name__ == '__main__':
-    pass
+    main()
