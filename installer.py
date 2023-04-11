fb0516b689a81b945cebb5bef9946d5311ea09b9
Gaku Tokusumi <gakutokusumi@mac.com>
2023-04-11 13:05:25 -0400

    setting up installer file

--- installer.py
+++ installer.py
@@ -1,6 +1,6 @@
 '''
 This file should allow for easier updating as you can download .py files and adjust if needed and create executable locally instead of downloading.
-Will just need to convert this file into an executable.
+Will just need to convert this file into an executable. Should be able to regularly update after downloading updated .py files from github.
 '''
 
 
@@ -14,10 +14,15 @@ def checkRoomFile():
     pass
 
 # creates room file if not found. Will get data for room credit and what room types S/O & C/O equal that credit type
-# Note for me: maybe use the excel sheet to grab data during script to be a universal. (might need to standardize suite values)
+# Note for me: maybe use the excel sheet to grab data during script to be a universal. (might need to standardize suite values? Could cause the script to take a littl bit longer)
+#                                                                                      (Especially for bigger hotels with more rooms.)
 def createRoomFile():
     pass
 
 # use pyintaller to create executable. Should make it easier to allow for updates later on
 def createExe():
+    pass
+
+# move executable file from C:/user/username/output to C:/user/Desktop for easier access. 
+def moveFile():
     pass
