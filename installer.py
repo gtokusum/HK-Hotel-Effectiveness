ac8fd2dff91accca14fa0efa60243421b09788cc
Gaku Tokusumi <gakutokusumi@mac.com>
2023-04-11 12:42:20 -0400

    setting up installer.py

--- installer.py
+++ installer.py
@@ -1,1 +1,16 @@
+import pyinstaller
+import os
 
+
+# checks for file containing room type/credit data. 
+def checkRoomFile():
+    pass
+
+# creates room file if not found. Will get data for room credit and what room types S/O & C/O equal that credit type
+# Note for me: maybe use the excel sheet to grab data during script to be a universal. (might need to standardize suite values)
+def createRoomFile():
+    pass
+
+# use pyintaller to create executable. Should make it easier to allow for updates later on
+def createExe():
+    pass
